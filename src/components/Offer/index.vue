f0a507c0a1d02a67422bbc5e892aab7c361342d6
Petro  Gomes <petrogomes@camilles-mbp.speedport_w_723v_1_47_000>
2020-02-16 00:01:50 +0100

    Added Offer component

---
+++ src/components/Offer/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-if="isActive">
+    <b-field>
+      <b-input
+        v-model="inputValue"
+        type="number"
+        :placeholder="text"
+        required
+        validation-message="Invalid number"
+      ></b-input>
+    </b-field>
+    <b-button :disabled="inputValue===''" @click="() => onSubmit(inputValue)">Submit</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isActive: Boolean,
+    text: {
+      type: String,
+      required: true,
+    },
+    onSubmit: {
+      type: Function,
+      required: true,
+    },
+  },
+  data: () => ({
+    inputValue: '',
+  }),
+}
+</script>
